5c364d799ca86a044fc0fdffe6845d1c6c0a815f
Oleg Strelnikov <oleg.strelnikov@gmail.com>
2020-03-08 02:00:04 +0400

    implemented pretty matrix printing

--- matrix-print.h
+++ matrix-print.h
@@ -10,8 +10,43 @@
 
 #include <iostream>
 #include <iomanip>
+#include <sstream>
 
 #include "matrix.h"
+
+struct PrintedMatrix {
+	std::vector<size_t> widths;
+	std::vector<std::vector<std::string>> cells;
+	size_t max_width;
+	PrintedMatrix(size_t height, size_t width) : widths(width), cells(height), max_width() {
+		for (auto& row: cells) {
+			row.reserve(width);
+		}
+	}
+};
+
+template<typename T>
+static inline PrintedMatrix get_widths(matrix<T> const& m) {
+	auto const m_height = m.height();
+	auto const m_width = m.width();
+	PrintedMatrix result(m_height, m_width);
+	std::ostringstream oss;
+	size_t size;
+	for (auto i = decltype(m_height){}; i < m_height; ++i) {
+		for (auto j = decltype(m_width){}; j < m_width; ++j) {
+			oss.str("");
+			oss << m[i][j];
+			size = result.cells[i].emplace_back(oss.str()).size();
+			if (size > result.widths[j]) {
+				result.widths[j] = size;
+				if (size > result.max_width) {
+					result.max_width = size;
+				}
+			}
+		}
+	}
+	return result;
+}
 
 template<typename Container, size_t width = 10, char lb = '[', char rb = ']'>
 inline std::ostream& print_vector(Container const& v) {
@@ -26,22 +61,27 @@ inline std::ostream& print_vector(Container const& v) {
 	return std::cout;
 }
 
-template<typename T, size_t width = 10, char lb = '[', char rb = ']', char sep = ' '>
+template<typename T, char lb = '[', char rb = ']', char sep = ' '>
 inline std::ostream& print_matrix(matrix<T> const& m) {
+	std::ostream& os = std::cout;
+	auto r = get_widths(m);
 	auto const m_height = m.height();
 	auto const m_width = m.width();
+	const std::string ws(r.max_width, ' ');
 	if (m_height > 0) {
 		for (auto i = decltype(m_height){}; i < m_height; ++i) {
-			std::cout << lb << sep;
+			os << lb << sep;
+			auto& row = r.cells[i];
 			for (auto j = decltype(m_width){}; j < m_width; ++j) {
-				std::cout << std::setw(width) << m[i][j] << sep;
+				auto& cell = row[j];
+				os.write(ws.data(), r.widths[j] - cell.size()).write(cell.data(), cell.size()) << sep;
 			}
-			std::cout << rb << '\n';
+			os << rb << '\n';
 		}
 	} else {
-		std::cout << lb << rb << '\n';
+		os << lb << rb << '\n';
 	}
-	return std::cout;
+	return os;
 }
 
 template<typename L, typename R, size_t width = 10, char lb = '[', char rb = ']', char sep = ' '>
