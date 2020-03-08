6eaf3b32f53d9be2abd716e2cbf3e3bf6d74b43d
Oleg Strelnikov <oleg.strelnikov@gmail.com>
2020-03-08 06:27:19 +0400

    fixed trailing characters

--- matrix-print.h
+++ matrix-print.h
@@ -13,17 +13,18 @@
 #include <sstream>
 #include <algorithm>
 #include <numeric>
+#include <cassert>
 
 #include "matrix.h"
 
 namespace {
 
-struct PrintedMatrix {
+struct RenderedMatrix {
 	std::vector<size_t> widths;
 	std::vector<std::vector<std::string>> cells;
 	size_t max_width;
 	char trailer;
-	PrintedMatrix(size_t height, size_t width, char t) :
+	RenderedMatrix(size_t height, size_t width, char t) :
 		widths(width), cells(height ? height : 1), max_width(width > 0 ? width + 1 : 2), trailer(t) {
 		for (auto& row: cells) {
 			row.reserve(width);
@@ -32,10 +33,10 @@ struct PrintedMatrix {
 };
 
 template<typename T>
-inline PrintedMatrix get_widths(matrix<T> const& m, char trailer = ' ') {
+inline RenderedMatrix render(matrix<T> const& m, char trailer = ' ') {
 	auto const m_height = m.height();
 	auto const m_width = m.width();
-	PrintedMatrix result(m_height, m_width, trailer);
+	RenderedMatrix result(m_height, m_width, trailer);
 	std::ostringstream oss;
 	size_t size;
 	for (auto i = decltype(m_height){}; i < m_height; ++i) {
@@ -70,28 +71,34 @@ inline std::ostream& print_row(std::vector<std::string> const& row, std::vector<
 	return os << rb;
 } //print_row()
 
-inline std::ostream& print_rows(std::vector<PrintedMatrix> const& r, size_t i, const char* ws, char sep, char lb, char rb, char msep, std::ostream& os) {
-	for (auto const& rr: r) {
-		if (rr.cells.size() > i) {
-			print_row(rr.cells[i], rr.widths, ws, sep, lb, rb, os);
-		} else {
-			os.write(ws, rr.max_width);
-		}
-		os << msep; //todo: trailing msep
+
+inline std::ostream& print_row(RenderedMatrix const& rr, size_t row_number, const char* ws, char sep, char lb, char rb, char msep, std::ostream& os) {
+	if (rr.cells.size() > row_number) {
+		print_row(rr.cells[row_number], rr.widths, ws, sep, lb, rb, os);
+	} else {
+		os.write(ws, rr.max_width); //todo: trailing white spaces
 	}
-	return os << '\n';
+	return os;
 }
 
-inline std::ostream& print_matrices(std::vector<PrintedMatrix> const& r, char sep, char lb, char rb, char msep, std::ostream& os) {
-	auto const m_height = std::max_element(r.begin(), r.end(), [](auto const& lhs, auto const& rhs){return lhs.cells.size() < rhs.cells.size();})->cells.size();
-	const std::string ws(std::max_element(r.begin(), r.end(), [](auto const& lhs, auto const& rhs){return lhs.max_width < rhs.max_width;})->max_width, ' ');
-	for (auto const& rr: r) {
-		print_row(rr.cells[0], rr.widths, ws.data(), sep, lb, rb, os);
-		os << rr.trailer; //todo: trailing msep
+inline std::ostream& print_rows(std::vector<RenderedMatrix> const& r, size_t row_number, const char* ws, char sep, char lb, char rb, char msep, std::ostream& os) {
+	assert(r.size() > 0);
+	auto const last_matrix = r.size() - 1;
+	for (auto k = decltype(last_matrix){}; k < last_matrix; ++k) {
+		char trailers[2] = {r[k].trailer, msep};
+		print_row(r[k], row_number, ws, sep, lb, rb, msep, os).put(trailers[row_number > 0]);
 	}
-	os << '\n';
-	for (auto i = decltype(m_height){1}; i < m_height; ++i) {
-		print_rows(r, i, ws.data(), sep, lb, rb, msep, os);
+	print_row(r[last_matrix], row_number, ws, sep, lb, rb, msep, os);
+	return os << '\n';
+} //print_rows()
+
+inline std::ostream& print_matrices(std::vector<RenderedMatrix> const& r, char sep, char lb, char rb, char msep, std::ostream& os) {
+	if (!r.empty()) {
+		auto const m_height = std::max_element(r.begin(), r.end(), [](auto const& lhs, auto const& rhs){return lhs.cells.size() < rhs.cells.size();})->cells.size();
+		const std::string ws(std::max_element(r.begin(), r.end(), [](auto const& lhs, auto const& rhs){return lhs.max_width < rhs.max_width;})->max_width, ' ');
+		for (auto row_number = decltype(m_height){}; row_number < m_height; ++row_number) {
+			print_rows(r, row_number, ws.data(), sep, lb, rb, msep, os);
+		}
 	}
 	return os;
 } //print_matrices()
@@ -100,7 +107,7 @@ inline std::ostream& print_matrices(std::vector<PrintedMatrix> const& r, char se
 
 template<typename T>
 inline std::ostream& print_matrix(matrix<T> const& m, char sep = ' ', char lb = '[', char rb = ']', std::ostream& os = std::cout) {
-	return print_matrices(std::vector<PrintedMatrix>(1, get_widths(m)), sep, lb, rb, ' ', os);
+	return print_matrices(std::vector<RenderedMatrix>(1, render(m)), sep, lb, rb, '!', os);
 } //print_matrix()
 
 template<typename V>
@@ -111,7 +118,7 @@ inline std::ostream& print_vector(V const& v, char lb = '[', char rb = ']', std:
 template<typename L, typename R>
 inline std::ostream& print_matrix_multiplication(matrix<L> const& lhs, matrix<R> const& rhs,
 	char sep = ' ', char lb = '[', char rb = ']', char msep = ' ', std::ostream& os = std::cout) {
-	return print_matrices({get_widths(lhs, '*'), get_widths(rhs, '='), get_widths(lhs*rhs)}, sep, lb, rb, ' ', os);
+	return print_matrices({render(lhs, '*'), render(rhs, '='), render(lhs*rhs, '?')}, sep, lb, rb, ' ', os);
 } //print_matrix_multiplication()
 
 #endif /* MATRIX_PRINT_H_ */
